15bfde706b2fd2648ddf5339fe1a55b4218079cb
Hassan Alam <hassanalamdevs@gmail.com>
2020-10-26 22:04:52 +0500

    Learnt a new way to use logical operators and ternary operator

---
+++ processLoan.js
@@ -0,0 +1,13 @@
+// ----- Eligible for loan -----
+const monthlyIncome = 1_200;
+let hasHighIncome = monthlyIncome >= 1_200;
+
+let bankCredit = 14_550;
+let hasHighCredit = bankCredit >= 10_000;
+
+let eligibleForLoan = hasHighIncome && hasHighCredit;
+let applicationStatus = eligibleForLoan ? 'accepted' : 'rejected';
+
+console.log(`The candidate's application has been "${applicationStatus}!"`);
+// The following program elaborates the use of logical operators
+// , as well as ternary operator...
